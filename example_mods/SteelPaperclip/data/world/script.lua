51d7fc56c2b01090b8ba08e17afd5aed4a58fe92
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-08-19 10:42:33 +0100

    You haven't gone through true hell until you've tried to make a modchart using Psych Engine.
    Seriously, nothing fucking works. Fuck this stupid Psych Engine shit.

---
+++ example_mods/SteelPaperclip/data/world/script.lua
@@ -0,0 +1,5 @@
+function onCreate()
+	makeLuaText('fuckyou', 'Had to use Screen Shake instead of a modchart because Psych Engine sucks. Sorry.', 0, 275, 0)
+	setTextAlignment('fuckyou', 'center')
+	addLuaText('fuckyou')
+end
